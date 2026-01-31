f001dbff2cb0b8e9879e6e566ecd43366d405351
Nathan_Ipad <troloko8@gmail.com>
2026-01-31 17:25:40 +0200

    nth

--- react-learning-tool/src/App.tsx
+++ react-learning-tool/src/App.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import PortalExample from './components/Portal/PortalExample';
 import './App.css';
+// import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary.js';
+import ErrorComponentExample from './components/ErrorBoundary/ErrorComponentExample';
+import SuspenceExample from './components/SuspenseExample/SuspenseExample';
+import CustomSuspenceExample from './components/SuspenseExample/CustomSuspenseExample';
 
 function App() {
 
@@ -9,8 +13,9 @@ function App() {
     <div className="App">
       <main className="App-header">
         {/* <PortalExample />           */}
-
-
+        {/* <ErrorComponentExample /> */}
+        {/* <SuspenceExample /> */}
+        <CustomSuspenceExample />
       </main>
     </div>
   );
