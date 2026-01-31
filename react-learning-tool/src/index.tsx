82599bebcb0e5936ba312b5f411e7ab7baf6d4aa
Nathan_Ipad <troloko8@gmail.com>
2026-01-31 17:38:24 +0200

    hydratation

--- react-learning-tool/src/index.tsx
+++ react-learning-tool/src/index.tsx
@@ -4,14 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { hydrateRoot } from 'react-dom/client'
+import Counter from './components/hydratation/Cuounter';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
+// hydrateRoot(
+//   document.getElementById('root')!,
+//   <Counter />
+// )
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
