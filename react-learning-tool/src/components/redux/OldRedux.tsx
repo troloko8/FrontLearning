6e182c03e548d1a55cea07999e223526e8bb79c7
Nathan_Ipad <troloko8@gmail.com>
2026-02-18 19:15:53 +0200

    old good practices work with reducers

--- react-learning-tool/src/components/redux/OldRedux.tsx
+++ react-learning-tool/src/components/redux/OldRedux.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
+import { addCustomerAction, removeCustomerAction } from './store/customerReducer';
 
 const OldRedux: React.FC = () => {
 
     const count = useSelector((state: any) => state.count.count);
+    const customers = useSelector((state: any) => state.customers.customers );
 
     const dispatch = useDispatch();
 
@@ -16,11 +18,36 @@ const OldRedux: React.FC = () => {
         dispatch({ type: 'DECREMENT', payload: count});
     };
 
+    const addCustomer = (name: string) => {
+        const customer = {
+            name,
+            id: Date.now()
+        }
+        dispatch(addCustomerAction(name, customer));
+    };
+
+    const removeCustomer = (id: number) => {
+        dispatch(removeCustomerAction(id));
+    };
+
     return (
         <div>
             <h1>Count: {count}</h1>
             <button onClick={() => increment(1)}>Increment</button>
             <button onClick={() => decrement(1)}>Decrement</button>
+
+            <button onClick={() => removeCustomer(customers[customers.length - 1]?.id)}>Remove Customer</button>
+            <button onClick={() => addCustomer('Customer ' + (customers.length + 1))}>Add Customer</button>
+
+        {customers.length > 0 && (
+            <>
+                <h1>Customers: {customers.length}</h1>
+                {customers.map((customer: any) => <div key={customer.id}>{customer.name}</div>)}
+            </>
+        )}
+        {customers.length === 0 && (
+            <h1>No customers</h1>
+        )}
         </div>
     );
 };
--- react-learning-tool/src/components/redux/store/customerReducer.js
+++ react-learning-tool/src/components/redux/store/customerReducer.js
@@ -1,3 +1,4 @@
+import exp from "constants"
 
 const defaultState = {
     customers: []
@@ -6,10 +7,24 @@ const defaultState = {
 export const customerReducer = (state = defaultState, action) => {
     switch(action.type) {
         case 'ADD_CUSTOMER':
-        return { ...state, count: state.count + action.payload }
-        case 'GET_CUSTOMER':
-        return { ...state, count: state.count - action.payload }
+        return { ...state, customers: [...state.customers, action.payload] }
+        case 'REMOVE_CUSTOMER':
+        return { ...state, customers: state.customers.filter(customer => customer.id !== action.payload) }
         default:
         return state
+    }
+}
+
+export const addCustomerAction = (name, payload) => {
+    return {
+        type: 'ADD_CUSTOMER',
+        payload
+    }
+}
+
+export const removeCustomerAction = (id) => {
+    return {
+        type: 'REMOVE_CUSTOMER',
+        payload: id
     }
 }
